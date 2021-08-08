5ce967dc97293d847df2ca44b067f2a1b1ef6b48
lbluefishl <lbluefishl@yahoo.com>
2021-08-08 01:05:50 -0700

    calculator draft done

--- scripts.js
+++ scripts.js
@@ -1,27 +1,188 @@
-function add(a,b) {
-    return a + b;
+function add(a, b) {
+    display.innerHTML = +a + (+b);
+    firstNumber = display.innerHTML;
+    secondNumber = undefined;
+    operator = undefined;
+    beginSecond = undefined;
 };
 
-function subtract(a,b) {
-    return a - b;
+function subtract(a, b) {
+    display.innerHTML = +a - (+b);
+    firstNumber = display.innerHTML;
+    secondNumber = undefined;
+    operator = undefined;
+    beginSecond = undefined;
 };
 
-function multiply(a,b) {
-    return a * b;
+function multiply(a, b) {
+    display.innerHTML = +a * (+b);
+    firstNumber = display.innerHTML;
+    secondNumber = undefined;
+    operator = undefined;
+    beginSecond = undefined;
 };
 
-function divide(a,b) {
-    return a / b;
+function divide(a, b) {
+    display.innerHTML = +a / (+b);
+    firstNumber = display.innerHTML;
+    secondNumber = undefined;
+    operator = undefined;
+    beginSecond = undefined;
 }
 
-function operate(firstNumber, operator, secondNumber) {
-    if (operator = 'multiply') {
-        multiply(firstNumber, secondNumber);
-    } else if (operator = 'subtract') {
-        subtract(firstNumber, secondNumber);
-    } else if (operator = 'add') {
-        add(firstNumber, secondNumber);
-    } else if (operator = 'divide') {
-        divide(firstNumber, secondNumber);
+
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+let display = document.querySelector('div#display');
+const numbers = document.querySelectorAll('div.number');
+const clear = document.querySelector('#clear');
+const decimal = document.querySelector('#decimal');
+let operators = document.querySelectorAll('div.operator');
+let beginSecond;
+
+let finalOperators = document.querySelectorAll('div.equal');
+
+function displayFirstNumber(number) {
+    if (display.innerHTML.length < 10) {
+
+        display.innerHTML += number;
+
     }
-};
+}
+function displaySecondNumber(number) {
+    if (display.innerHTML.length < 10) {
+
+        display.innerHTML += number;
+
+    }
+}
+
+function clearDisplay() {
+    display.innerHTML = 0;
+    clearHighlight();
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
+    beginSecond = undefined;
+}
+
+clear.addEventListener('click', clearDisplay);
+
+
+for (i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function () {
+        clearHighlight();
+        if (display.innerHTML == 0 || (operator && !beginSecond)) {
+            display.innerHTML = this.id;
+        } else {
+
+            if (!(this.id == '.' && display.innerHTML.includes('.'))) {
+                addNumber(this.id);
+            }
+
+        }
+        modifyNumbers();
+    })
+}
+
+function modifyNumbers() {
+    if (operator) {
+        secondNumber = display.innerHTML;
+        beginSecond = true;
+    } else {
+        firstNumber = display.innerHTML;
+    }
+}
+
+
+function addNumber(number) {
+
+    displayFirstNumber(number);
+
+}
+
+for (i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', function () {
+        if (display.innerHTML != undefined & !operator) {
+            if (this.id === 'multiply') {
+                operator = 'multiply';
+            } else if (this.id === 'subtract') {
+                operator = 'subtract';
+            } else if (this.id === 'add') {
+                operator = 'add'
+            } else if (this.id === 'divide') {
+                operator = 'divide';
+            }
+            clearHighlight();
+            this.classList.add('highlighted');
+
+        }
+    })
+}
+
+function clearHighlight() {
+    for (i = 0; i < finalOperators.length; i++) {
+        finalOperators[i].classList.remove('highlighted');
+    }
+}
+
+for (i = 0; i < finalOperators.length; i++) {
+    finalOperators[i].addEventListener('click', function () {
+
+
+        if (firstNumber && secondNumber && operator) {
+
+            if (operator === 'add') {
+                operator = undefined;
+                add(firstNumber, secondNumber);
+            } else if (operator === 'subtract') {
+                operator = undefined;
+                subtract(firstNumber, secondNumber);
+            } else if (operator === 'multiply') {
+                operator = undefined;
+                multiply(firstNumber, secondNumber);
+            } else if (operator === 'divide') {
+                if (secondNumber == 0) {
+                    display.innerHTML = 'Not possible'
+                } else {
+                    operator = undefined;
+                    divide(firstNumber, secondNumber);
+                }
+            }
+            clearHighlight();
+            checkDigits();
+        }
+
+    })
+}
+
+
+for (i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', function () {
+        if (display.innerHTML != undefined & !operator) {
+            if (this.id === 'multiply') {
+                operator = 'multiply';
+            } else if (this.id === 'subtract') {
+                operator = 'subtract';
+            } else if (this.id === 'add') {
+                operator = 'add'
+            } else if (this.id === 'divide') {
+                operator = 'divide';
+            }
+            clearHighlight();
+            this.classList.add('highlighted');
+
+        }
+    })
+}
+
+function checkDigits() {
+    if (display.innerHTML.length >= 10 && display.innerHTML != 'Not possible') {
+        let num = +display.innerHTML;
+        num = num.toPrecision(9);
+        display.innerHTML = num;
+    }
+}
